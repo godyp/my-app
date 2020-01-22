af2a41980fa986ebd214612395477d49db23c82a
Godai Imazato <godai@sein.mse.kyutech.ac.jp>
2020-01-22 05:58:55 +0000

    selectorが変更されたとき拍手できる数と、された数を変更する(test)

--- src/Top.js
+++ src/Top.js
@@ -2,89 +2,104 @@ import React, { Component } from 'react';
 // import './App.css';
 
 class Top extends Component {
-  constructor(){
+constructor(){
     super();
     this.state = {
-      text: "",
-      hasTextError: false,
-      users:[{
+    text: "",
+    hasTextError: false,
+    selectedValue: 0,
+    crapPoint: 0,
+    getPoint: 0,
+    users:[{
         "id": 0,
         "name": "unknown",
         "image": "unknown.png",
         "give": 0,
         "get": 0
-      }]
+    }]
     }
-  }
+}
 
-  componentWillMount(){
+componentWillMount(){
     this.fetchTasks()
-  }
+}
 
-  fetchTasks(){
+fetchTasks(){
     fetch("http://localhost:3001/users") // データを取得しに行く
     .then( response => response.json() ) // json型のレスポンスをオブジェクトに変換する
     .then( json => { // オブジェクトに変換したレスポンスを受け取り、
-      this.setState({ users: json }) // Stateを更新する
+    this.setState({ users: json }) // Stateを更新する
     })
-  }
+}
 
-  handleTextChange(event){
+handleTextChange(event){
     const inputValue = event.target.value;
     const isOk = inputValue.length >= 5;
     this.setState({
-      text: inputValue,
-      hasTextError: isOk
+    text: inputValue,
+    hasTextError: isOk
     });
-  }
+}
 
-  render() {
+handleUserChange(event){
+    this.setState({
+        selectedValue: e.target.value,
+        crapPoint: this.state.users[e.target.value].give,
+        getPoint: this.state.users[e.target.value].get
+    })
+}
+
+render() {
     let button;
     if (this.state.hasTextError){
-      button = <input className="btn" type="submit" value="投稿" />;
+    button = <input className="btn" type="submit" value="投稿" />;
     }else{
-      button = <div className="no-btn">投稿</div>;
+    button = <div className="no-btn">投稿</div>;
     }
+
     return (
-      <div className="page-container">
+    <div className="page-container">
         <div className="top">
-          <div className="over">
-            <div className="user-container">
-              <img src={`${process.env.PUBLIC_URL}/images/man01.png`} alt="user"/>
-              <select defaultValue="0">
-                    {
-                        this.state.users.map( user => {
-                            return <option className="user" value={ user.id }>{ user.name }</option>
-                        })
-                    }
-              </select>
+            <div className="over">
+                <div className="user-container">
+                    <img src={`${process.env.PUBLIC_URL}/images/man01.png`} alt="user"/>
+                    <select
+                    vaue={this.state.selectedValue}
+                    onChange={(event)=>{this.handleUserChange(event)}}
+                    >
+                            {
+                                this.state.users.map( user => {
+                                    return <option className="user" value={ user.id }>{ user.name }</option>
+                                })
+                            }
+                    </select>
+                </div>
+                <div className="user-info">
+                        拍手できる：{this.state.crapPoint}　拍手された：{this.state.getPoint}
+                </div>
             </div>
-            <div className="user-info">
-              拍手できる：100　拍手された：0
+            <div className="under">
+                <div className="user-container">
+                    <img src={`${process.env.PUBLIC_URL}/images/man01.png`} alt="user"/>
+                    <select defaultValue="0">
+                        {
+                            this.state.users.map( user => {
+                                return <option className="user" value={ user.id }>{ user.name }</option>
+                            })
+                        }
+                    </select>
+                </div>
+                <div className="form-area">
+                    <form>
+                        <textarea value={this.state.text} onChange={(event)=>{this.handleTextChange(event)}}/>
+                        {button}
+                    </form>
+                </div>
             </div>
-          </div>
-          <div className="under">
-            <div className="user-container">
-              <img src={`${process.env.PUBLIC_URL}/images/man01.png`} alt="user"/>
-              <select defaultValue="0">
-                {
-                    this.state.users.map( user => {
-                        return <option className="user" value={ user.id }>{ user.name }</option>
-                    })
-                }
-              </select>
-            </div>
-            <div className="form-area">
-              <form>
-                <textarea value={this.state.text} onChange={(event)=>{this.handleTextChange(event)}}/>
-                {button}
-              </form>
-            </div>
-          </div>
         </div>
-      </div>
+    </div>
     )
-  }
+}
 }
 
 export default Top;
