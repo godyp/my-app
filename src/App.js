b19f8b4ef4f21d28adee63c36961f5233bb0a1f5
Godai Imazato <godai@sein.mse.kyutech.ac.jp>
2020-01-24 20:05:24 +0000

    投稿履歴のコンテンツを追加(test1-1)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Top from './Top';
+import Panel from './Panel';
 
 class App extends Component {
   render() {
     return(
-      <div><Top /></div>
+      <div>
+        <Top />
+        <Panel />
+      </div>
     )
   }
 }
