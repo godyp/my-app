6ddfe6a90d45a84f69d29798d24db90633491a68
Godai Imazato <godai@sein.mse.kyutech.ac.jp>
2020-01-25 16:00:09 +0000

    投稿履歴のコンテンツのデザイン修正(1-1)

--- src/Panel.js
+++ src/Panel.js
@@ -5,19 +5,21 @@ class Panel extends Component {
         return(
             <div className="contents-container">
                 <div className="direction">
-                    <img src={`${process.env.PUBLIC_URL}/images/man01}`} alt="user"/>
+                    <img src={`${process.env.PUBLIC_URL}/images/man01.png}`} alt="user"/>
                     <img src={`${process.env.PUBLIC_URL}/images/arrow.png`} alt="arrow"/>
-                    <img src={`${process.env.PUBLIC_URL}/images/woman01`} alt="user"/>
+                    <img src={`${process.env.PUBLIC_URL}/images/woman01.png`} alt="user"/>
                 </div>
                 <div className="message">
-                    <p>お疲れ様</p>
+                    <span>お疲れ様</span>
                 </div>
                 <div className="crapAndDate">
                     <div className="crapNum">
-                        <img src={`${process.env.PUBLIC_URL}/images/crap.png`} alt="crap"/>
+                        <img class="crap-img" src={`${process.env.PUBLIC_URL}/images/crap.png`} alt="crap"/>
                         <span>10</span>
                     </div>
-                    <div className="submit-time">2020/01/26 00:41</div>
+                    <div className="submit-time">
+                        <span>2020/01/26 00:41</span>
+                    </div>
                 </div>
             </div>
         )
