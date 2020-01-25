06aaa8bfd2dd6295ec623300978cb596d7b45184
Godai Imazato <godai@sein.mse.kyutech.ac.jp>
2020-01-25 15:39:50 +0000

    投稿履歴のコンテンツを追加(test1-7)

--- src/Panel.js
+++ src/Panel.js
@@ -1,58 +1,25 @@
 import React, { Component } from 'react';
 
 class Panel extends Component {
-    constructor() {
-        super()
-        // this.submitTask = this.submitTask.bind(this)
-        this.fetchUsers = this.fetchUsers.bind(this)
-    }
-
-    componentWillMount(){
-        this.fetchUsers()
-        this.fetchContents()
-    }
-
-    fetchUsers(){
-        fetch("http://localhost:3001/users") // データを取得しに行く
-        .then( response => response.json() ) // json型のレスポンスをオブジェクトに変換する
-        .then( json => { // オブジェクトに変換したレスポンスを受け取り、
-        this.setState({ users: json }) // Stateを更新する
-        })
-    }
-
-    fetchContents(){
-        fetch("http://localhost:3001/contents") // データを取得しに行く
-        .then( response => response.json() ) // json型のレスポンスをオブジェクトに変換する
-        .then( json => { // オブジェクトに変換したレスポンスを受け取り、
-        this.setState({ contents: json }) // Stateを更新する
-        })
-    }
-
     render() {
         return(
-        <div>
-            {this.state.contents.map( content => {
-                return (
-                    <div className="contents-container">
-                        <div className="direction">
-                            <img src={`${process.env.PUBLIC_URL}/images/${this.state.users[content.from].image}`} alt="user"/>
-                            <img src={`${process.env.PUBLIC_URL}/images/arrow.png`} alt="arrow"/>
-                            <img src={`${process.env.PUBLIC_URL}/images/${this.state.users[content.to].image}`} alt="user"/>
-                        </div>
-                        <div className="message">
-                            <p>{content.message}</p>
-                        </div>
-                        <div className="crapAndDate">
-                            <div className="crapNum">
-                                <img src={`${process.env.PUBLIC_URL}/images/crap.png`} alt="crap"/>
-                                <span>{content.craped}</span>
-                            </div>
-                            <div className="submit-time">{content.submitTime}</div>
-                        </div>
+            <div className="contents-container">
+                <div className="direction">
+                    <img src={`${process.env.PUBLIC_URL}/images/man01}`} alt="user"/>
+                    <img src={`${process.env.PUBLIC_URL}/images/arrow.png`} alt="arrow"/>
+                    <img src={`${process.env.PUBLIC_URL}/images/woman01`} alt="user"/>
+                </div>
+                <div className="message">
+                    <p>お疲れ様</p>
+                </div>
+                <div className="crapAndDate">
+                    <div className="crapNum">
+                        <img src={`${process.env.PUBLIC_URL}/images/crap.png`} alt="crap"/>
+                        <span>10</span>
                     </div>
-                )
-            })}
-        </div>
+                    <div className="submit-time">2020/01/26 00:41</div>
+                </div>
+            </div>
         )
     }
 }
