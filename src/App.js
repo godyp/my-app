3a60be82c1c466e255c33c3832b6f60c6e8f43d7
Godai Imazato <godai@sein.mse.kyutech.ac.jp>
2020-01-25 17:15:44 +0000

    コンテンツ一覧を表示(1-1)

--- src/App.js
+++ src/App.js
@@ -4,11 +4,72 @@ import Top from './Top';
 import Panel from './Panel';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      from: 0,
+      to: 0,
+      users:[{
+          "id": 0,
+          "name": "unknown",
+          "image": "unknown.png",
+          "give": 0,
+          "get": 0
+      }],
+      contents:[{
+          "id": 0,
+          "from": 0,
+          "to": 0,
+          "message": "---",
+          "craped": "0",
+          "submitTime": "---"
+      }]
+    }
+    this.fetchUsers = this.fetchUsers.bind(this)
+    this.fetchContents = this.fetchContents.bind(this)
+}
+
+componentWillMount(){
+    this.fetchUsers()
+    this.fetchContents()
+}
+
+fetchUsers(){
+    fetch("http://localhost:3001/users") // データを取得しに行く
+    .then( response => response.json() ) // json型のレスポンスをオブジェクトに変換する
+    .then( json => { // オブジェクトに変換したレスポンスを受け取り、
+    this.setState({ users: json }) // Stateを更新する
+    })
+}
+
+fetchContents(){
+    fetch("http://localhost:3001/contents") // データを取得しに行く
+    .then( response => response.json() ) // json型のレスポンスをオブジェクトに変換する
+    .then( json => { // オブジェクトに変換したレスポンスを受け取り、
+    this.setState({ contents: json }) // Stateを更新する
+    })
+}
+
   render() {
     return(
       <div>
         <Top />
-        <Panel />
+
+        {this.state.contents.map((content)=>{
+          this.setState({
+            from: content.from,
+            to: content.to
+          })
+          return (
+            <Panel
+            fromUserImage = {this.state.users[this.state.from].image}
+            toUserImage   = {this.state.users[this.state.to].image}
+            message       = {content.message}
+            craped        = {content.craped}
+            submitTime    = {content.submitTime}
+            />
+          )
+        })}
       </div>
     )
   }
--- src/Panel.js
+++ src/Panel.js
@@ -1,70 +1,24 @@
 import React, { Component } from 'react';
 
 class Panel extends Component {
-    constructor() {
-        super()
-        this.state = {
-            from: 0,
-            to: 0,
-            users:[{
-                "id": 0,
-                "name": "unknown",
-                "image": "unknown.png",
-                "give": 0,
-                "get": 0
-            }],
-            contents:[{
-                "id": 0,
-                "from": "0",
-                "to": "0",
-                "message": "---",
-                "craped": "0",
-                "submitTime": "---"
-            }]
-        }
-        this.fetchUsers = this.fetchUsers.bind(this)
-        this.fetchContents = this.fetchContents.bind(this)
-    }
-
-    componentWillMount(){
-        this.fetchUsers()
-        this.fetchContents()
-    }
-
-    fetchUsers(){
-        fetch("http://localhost:3001/users") // データを取得しに行く
-        .then( response => response.json() ) // json型のレスポンスをオブジェクトに変換する
-        .then( json => { // オブジェクトに変換したレスポンスを受け取り、
-        this.setState({ users: json }) // Stateを更新する
-        })
-    }
-
-    fetchContents(){
-        fetch("http://localhost:3001/contents") // データを取得しに行く
-        .then( response => response.json() ) // json型のレスポンスをオブジェクトに変換する
-        .then( json => { // オブジェクトに変換したレスポンスを受け取り、
-        this.setState({ contents: json }) // Stateを更新する
-        })
-    }
-
     render() {
         return(
             <div className="contents-container">
                 <div className="direction">
-                    <img className="person-img" src={`${process.env.PUBLIC_URL}/images/${this.state.users[this.state.from].image}`} alt="user"/>
+                    <img className="person-img" src={`${process.env.PUBLIC_URL}/images/${this.props.fromUserImage}`} alt="user"/>
                     <img className="arrow-img" src={`${process.env.PUBLIC_URL}/images/arrow.png`} alt="arrow"/>
-                    <img className="person-img" src={`${process.env.PUBLIC_URL}/images/${this.state.users[this.state.to].image}`} alt="user"/>
+                    <img className="person-img" src={`${process.env.PUBLIC_URL}/images/${this.props.toUserImage}`} alt="user"/>
                 </div>
                 <div className="message">
-                    <span>{this.state.contents[0].message}</span>
+                    <span>{this.props.message}</span>
                 </div>
                 <div className="crapAndDate">
                     <div className="crapNum">
                         <img className="crap-img" src={`${process.env.PUBLIC_URL}/images/crap.png`} alt="crap"/>
-                        <span>{this.state.contents[0].craped}</span>
+                        <span>{this.props.craped}</span>
                     </div>
                     <div className="submit-time">
-                        <span>{this.state.contents[0].submitTime}</span>
+                        <span>{this.props.submitTime}</span>
                     </div>
                 </div>
             </div>
